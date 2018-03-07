85ba0514643d6d7cc06b4ce14a911bd651702d85
deepsh10 <shaw@shaw>
2018-03-07 23:41:56 +0530

    Done

--- Node_Related_Issues/Paired10LL.cpp
+++ Node_Related_Issues/Paired10LL.cpp
@@ -14,6 +14,31 @@ struct Node* newNode(int data)
     return temp;
 }
 
+void findPairsOf10(Node *start)
+{
+    int length=0;
+    int index,match;
+    
+    struct Node* temp = start;
+    struct Node* next = NULL;
+    
+    while(temp->data != -1)
+    {
+        next = temp->next;
+        while(next->data  != -1)
+        {
+            length++;
+            if(temp->data + next->data == 10)
+            {
+                printf("(%d,%d)\n",temp->data,next->data);
+            }
+            next = next->next;
+        }
+        temp = temp->next;
+    }
+    return;
+}
+
 int main(int argc, char** argv) {
     struct Node* n1 = newNode(0);
     n1->next = newNode(2);
@@ -24,7 +49,10 @@ int main(int argc, char** argv) {
     n1->next->next->next->next->next= newNode(10);
     n1->next->next->next->next->next->next = newNode(20);
     n1->next->next->next->next->next->next->next = newNode(-10);
-    n1->next->next->next->next->next->next->next->next->data=-1;
+    n1->next->next->next->next->next->next->next->next = newNode(-1);
+    
+    findPairsOf10(n1);
+    
     return 0;
 }
 
