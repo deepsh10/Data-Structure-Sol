172acd51230d43da24a9402d40d2062f9da790fe
shaw <shaw@shaw>
2018-03-08 23:01:27 +0530

    Inital Version

---
+++ findingEndOfNodes.cpp
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   findingEndOfNodes.cpp
+ * Author: shaw
+ *
+ * Created on March 8, 2018, 10:42 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+struct Node* newNode(int data)
+{
+    struct Node* temp = new Node();
+    
+    return temp;
+}
+
+struct Node
+{
+    int data;
+    Node *right=NULL,*left=NULL;
+};
+
+int main(int argc, char** argv) {
+    
+    
+    return 0;
+}
+
